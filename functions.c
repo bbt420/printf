73463e932a8f2692fc3964dd9e0a2bc633f813b7
chinonsopaul <66165403+chinonsopaul@users.noreply.github.com>
2023-03-26 20:34:45 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -106,6 +106,7 @@ int print_percent(va_list types, char buffer[],
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
+ * good job brother
  * Return: Number of chars printed
  */
 int print_int(va_list types, char buffer[],
