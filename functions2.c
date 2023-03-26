b0c5f251bec12bdfa216b4428e47376c4931a87a
chinonsopaul <66165403+chinonsopaul@users.noreply.github.com>
2023-03-26 20:40:11 +0100

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -3,6 +3,7 @@
 /****************** PRINT POINTER ******************/
 /**
  * print_pointer - Prints the value of a pointer variable
+ * Handle the following custom conversion specifier:
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
