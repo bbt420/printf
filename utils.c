2906b6c865ad32c1f8ba95efb0ab2985a5b2bd63
chinonsopaul <66165403+chinonsopaul@users.noreply.github.com>
2023-03-26 20:48:13 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -3,7 +3,7 @@
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
- *
+ * Handle the 0 flag character for non-custom conversion specifiers.
  * Return: 1 if c is printable, 0 otherwise
  */
 int is_printable(char c)
